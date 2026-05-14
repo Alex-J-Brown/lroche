3482f94d44ae935fa51d3a8341c1f227df8724f7
Alex-J-Brown <alexjbrown.astro@gmail.com>
2026-05-14 21:58:41 +0200

    update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub mod numface;
 pub mod pparam;
 pub mod set_bright_spot_grid;
 pub mod set_cyclotron_grid;
+pub mod set_cyclotron_continuum;
 pub mod set_disc_continuum;
 pub mod set_disc_grid;
 pub mod set_star_continuum;
