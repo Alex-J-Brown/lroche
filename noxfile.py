8191317a4a32208e7babc22df1a4ccc98ace1b07
Stuart Littlefair <s.littlefair@shef.ac.uk>
2026-08-18 20:32:01 +0100

    bugfixes in CI

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,8 @@
 import nox
 
 
-@nox.session(python=["3.9", "3.13"])
-def test(session):
+@nox.session(python=["3.10", "3.11", "3.12"])
+def tests(session):
     # Install dependencies
     session.install(".[tests]")
     # Run tests
