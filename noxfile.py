9725a1e06caebf30cbfac5cc56890150700165a5
Stuart Littlefair <s.littlefair@shef.ac.uk>
2026-08-18 20:34:37 +0100

    [skip_ci] reduce the number of tests run

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.10", "3.11", "3.12"])
+@nox.session(python=["3.11", "3.12"])
 def tests(session):
     # Install dependencies
     session.install(".[tests]")
