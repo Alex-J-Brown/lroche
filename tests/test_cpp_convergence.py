f4de18a0fe483b2fd2a2869264066d2fd6678146
Felix Teutloff <astro.felix@teutloff.org>
2026-08-20 10:41:33 +0200

    Changed structuring of files in convergence tests and adapted test_cpp_convergence.py accordingly/ Added small README.md for contributing
    I've changed the file structure such that modfiles and solutions of a single object are always lumped together, and separated into dictionaries named after the object. This allows us to use pytest.mark.parametrize to automate the tests a little better.
    
    I've also added a short destription of the directory and how to contribute to the testing suite.
    
    Removed __pycache__ directory because it was wrongly included in tracking due to blanket `git add`.

--- tests/test_cpp_convergence.py
+++ tests/test_cpp_convergence.py
@@ -1,59 +1,44 @@
-import lcurve
-import numpy as np
-import pytest
+from __future__ import annotations
 
-def compare_cpp_rust_solution(
-    modfile: str,
-    cpp_solution_file: str
-) -> None:
+import os
+import pytest
+import numpy as np
+
+import lcurve
+
+
+# Setup function. If everything goes well, this should not be modified.
+def fetch_object_files() -> list[tuple[str, str]]:
+    # Make a list of all directories that are in a given directory, omitting __pycache__
+    list_directories_full_path = lambda path: [
+        os.path.join(path, d)
+        for d in os.listdir(path)
+        if os.path.isdir(os.path.join(path, d)) and d != "__pycache__"
+    ]
+
+    # Do the above for the `./tests/` directory
+    files = list_directories_full_path("./tests/")
+
+    # Make tuples of modfile cpp_solution file combinations for each directory
+    return [(dir + "/modfile.mod", dir + "/cpp_solution.dat") for dir in files]
+
+
+@pytest.mark.parametrize("modfile, cpp_solution_file", fetch_object_files())
+def test_compare_cpp_rust_solution(modfile: str, cpp_solution_file: str) -> None:
 
     # Load the mod file and the solution
     binary_model = lcurve.BinaryModel.from_file(modfile)
     cpp_solution = np.loadtxt(cpp_solution_file)
 
     # Prepare inputs and cpp flux measurements
-    time = np.ascontiguousarray(cpp_solution[:,0])
-    flux = np.ascontiguousarray(cpp_solution[:,1])
-    t_exp = np.ones_like(time) * 1/len(time)
+    time = np.ascontiguousarray(cpp_solution[:, 0])
+    flux = np.ascontiguousarray(cpp_solution[:, 1])
+    t_exp = np.ones_like(time) * 1 / len(time)
     n_div = np.ones_like(time)
 
     # Calculate _Rust_ light curve
-    lc = binary_model.compute_light_curve(
-        time,
-        t_exp,
-        n_div
-    )
+    lc = binary_model.compute_light_curve(time, t_exp, n_div)
 
     # Compare the Rust and C++ solution
-    diff = lc.total - flux
     # Use approximate values of the difference to avoid floating point problems
-    assert pytest.approx(diff) == np.zeros_like(diff)
-    
-    return None
-
-def test_PTF1J0823():
-    compare_cpp_rust_solution(
-        "tests/modfiles/PTF1J0823+0819.mod",
-        "tests/cpp_solutions/PTF1J0823+0819.dat"
-    )
-    return None
-
-def test_FBS_2303_tess():
-    compare_cpp_rust_solution(
-        "tests/modfiles/FBS_2303+344_TESS.mod",
-        "tests/cpp_solutions/FBS_2303+344_TESS.dat"
-    )
-    return None
-
-def test_FBS_2303_zr():
-    compare_cpp_rust_solution(
-        "tests/modfiles/FBS_2303+344_zr.mod",
-        "tests/cpp_solutions/FBS_2303+344_zr.dat"
-    )
-    return None
-def test_PG0941():
-    compare_cpp_rust_solution(
-        "tests/modfiles/PG0941+280.mod",
-        "tests/cpp_solutions/PG0941+280.dat"
-    )
-    return None
+    assert lc.total == pytest.approx(flux, abs=1e-16)
